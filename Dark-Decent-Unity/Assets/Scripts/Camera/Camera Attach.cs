94c75548306880b3700e044b1296ccc0f7834866
gvidas <danieliusgvidas@gmail.com>
2023-06-23 17:52:05 +0100

    betterised head bobbing

--- Dark-Decent-Unity/Assets/Scripts/Camera/Camera Attach.cs
+++ Dark-Decent-Unity/Assets/Scripts/Camera/Camera Attach.cs
@@ -11,9 +11,13 @@ public class CameraAttach : MonoBehaviour
     public float Amplitude = 0.015f;
     public float Frequency = 10f;
 
-    void Update()
-    {
-	transform.position = AttachTo.position;
-	transform.position += new Vector3(Mathf.Sin(Time.time * Frequency) * Amplitude, Mathf.Cos(Time.time * Frequency) * Amplitude, 0f);
+    void Update() {
+		transform.position = AttachTo.position;
+		//ur code sucks new Vector3(Mathf.Sin(Time.time * Frequency) * Amplitude, Mathf.Cos(Time.time * Frequency) * Amplitude, 0f);
+		Vector3 displacement = Vector3.zero;
+		displacement.y += Mathf.Sin(Time.time * Frequency) * Amplitude;
+		displacement.x += Mathf.Cos(Time.time * Frequency / 2) * Amplitude * 2;
+	    
+	    transform.position += displacement;
     }
 }
